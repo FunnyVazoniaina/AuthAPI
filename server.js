33d8340d06cc2d1a207da038bb2b26a4ccd23c11
Funny VAZONIAINA <funnyvazoniaina@gmail.com>
2025-06-08 13:58:58 +0200

    Adding form-data parser in the server

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require("express");
 const connectDB = require("./config/db");
 const userRoutes = require("./routes/user.routes");
+
+require("dotenv").config();
+
+//Initialize express app
+const app = express();
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Middleware to parse URL-encoded bodies like form submissions
+app.use(express.urlencoded({ extended: true }));
+
 // Connect to MongoDB
 connectDB();
-const app = express();
-require("dotenv").config();
+
 app.listen(process.env.PORT, () => {
   console.log("Server is running on port " + process.env.PORT);
 });
+
+// defining route
+app.use("/api/users", userRoutes);
