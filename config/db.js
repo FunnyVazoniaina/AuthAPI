f6a68c97655ceb7a02284e635c383a9a74039740
Funny VAZONIAINA <funnyvazoniaina@gmail.com>
2025-06-08 07:09:25 +0200

    Setting script for database connection

--- config/db.js
+++ config/db.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      useCreateIndex: true
-    });
-    console.log('MongoDB connected successfully');
-  } catch (error) {
-    console.error('MongoDB connection failed:', error.message);
-    process.exit(1); // Exit process with failure
-  }
-}   
+const dotenv = require('dotenv');
+dotenv.config(); // Load environment variables from .env file
+
+const connectDB = () => {
+ 
+  mongoose.connect(process.env.MONGO_URI, {})
+  .then(() => {
+    console.log('Hell YEAH! You re connected to MongoDB successfully');
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+  });
+}
 module.exports = connectDB;
