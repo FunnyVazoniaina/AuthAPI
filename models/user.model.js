753107f2c64641d60f9372b3496fe48b9e076544
Funny VAZONIAINA <funnyvazoniaina@gmail.com>
2025-06-08 10:15:50 +0200

    Setting userModel

--- models/user.model.js
+++ models/user.model.js
@@ -1,26 +1,30 @@
-const mongoose = require('mongoose');
-const userSchema = new mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 6,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  password: {
-    type: String,
-    required: true,
-    minlength: 6
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-    trim: true
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now
+  {
+    timestamps: true,
   }
-});
-const User = mongoose.model('User', userSchema);
-module.exports = User;
+);
+module.exports = mongoose.model("User", userSchema);
